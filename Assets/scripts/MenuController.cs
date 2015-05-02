588c53741c5d499940006777bd23d02c7c7b533e
Sven Lauterbach <SvenLauterbach@live.de>
2015-05-02 15:51:42 +0200

    MainMenu added

---
+++ Assets/scripts/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void ClickButton(string buttonID)
+    {
+        Application.LoadLevel("level");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
