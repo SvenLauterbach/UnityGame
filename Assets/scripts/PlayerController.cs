12162e3fc5a13a5de05ddc1a0012a487502b7ec3
Sven Lauterbach <SvenLauterbach@live.de>
2015-05-02 14:33:53 +0200

    cleanup boardmanager and added player controller

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+	    Rigidbody2D rig = this.GetComponent<Rigidbody2D>();
+
+        rig.MovePosition(new Vector2(hor, ver));
+	}
+}
