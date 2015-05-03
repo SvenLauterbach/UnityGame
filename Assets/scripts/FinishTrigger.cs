7f83cc3a19c6b116c8558267e2d5495d65fb34d0
Sven Lauterbach <SvenLauterbach@live.de>
2015-05-03 03:34:05 +0200

    FinishTrigger

---
+++ Assets/scripts/FinishTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishTrigger : MonoBehaviour
+{
+
+    public Player Player;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Test");
+        GameManager.instance.ShowFinishMenu();
+        Player.Stop();
+    }
+}
